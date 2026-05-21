73d176f6052210325882a9f145434c832d2cc486
litzzzhang <litzzzhang@gmail.com>
2026-05-21 20:43:40 +0800

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- ~/.config/nvim/lua/config/options.lua
-local opt = vim.opt
-
-opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.g.clipboard = "osc52"
